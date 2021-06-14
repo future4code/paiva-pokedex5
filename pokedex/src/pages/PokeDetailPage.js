25f234da15aaf8c64af3e438cee4afb06a519ea8
BrunoSilva28 <brunosantosasilva@gmail.com>
2021-06-14 16:43:40 -0300

    iniciando projeto

---
+++ pokedex/src/pages/PokeDetailPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokeDetailPage = () => {
+  return (
+    <div>
+        Estou na PokeDetail
+    </div>
+  );
+}
+
+export default PokeDetailPage;
---
+++ pokedex/src/routes/Coordinator.js
@@ -0,0 +1,7 @@
+export const goToHomePage = (history) => {
+    history.push("/");
+};
+
+export const goToPokedexPage = (history) => {
+    history.push("/pokedex");
+};
